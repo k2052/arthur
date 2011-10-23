caa148d33e4a2b08836446d6fafb3028f5f6f99c
Ken Erickson <bookworm.productions@gmail.com>
2011-10-22 17:51:33 -0700

    redis

--- storage/cache/adapter/Redis.php
+++ storage/cache/adapter/Redis.php
@@ -2,81 +2,71 @@
 
 namespace arthur\storage\cache\adapter;
 
-use Memcached;
-use arthur\util\Set;
+use Redis as RedisCore;
 
-class Memcache extends \arthur\core\Object 
+class Redis extends \arthur\core\Object 
 {
-	const CONN_DEFAULT_PORT = 11211;
-	public $connection = null;
+	public $connection;
 
 	public function __construct(array $config = array()) 
 	{
 		$defaults = array(
-			'expiry' => '+1 hour',
-			'host'   => '127.0.0.1'
-		);          
+			'host'       => '127.0.0.1:6379',
+			'expiry'     => '+1 hour',
+			'persistent' => false
+		); 
 		
-		parent::__construct(Set::merge($defaults, $config));
+		parent::__construct($config + $defaults);
 	}
-	
+
 	protected function _init() 
 	{
-		$this->connection = $this->connection ?: new Memcached();
-		$servers          = array();
+		if(!$this->connection)
+			$this->connection = new RedisCore();
 
-		if(isset($this->_config['servers'])) {
-			$this->connection->addServers($this->_config['servers']);
-			return;
-		}  
-		
-		$this->connection->addServers($this->_formatHostList($this->_config['host']));
+		list($ip, $port) = explode(':', $this->_config['host']);
+		$method = $this->_config['persistent'] ? 'pconnect' : 'connect';
+		$this->connection->{$method}($ip, $port);
 	}
 
-	protected function _formatHostList($host) 
+
+	protected function _ttl($key, $expiry) 
 	{
-		$fromString = function($host) 
-		{
-			if(strpos($host, ':')) {
-				list($host, $port) = explode(':', $host);
-				return array($host, intval($port));
-			}      
-			
-			return array($host, Memcache::CONN_DEFAULT_PORT);
-		};
-
-		if(is_string($host))
-			return array($fromString($host));
-		$servers = array();
-
-		while(list($server, $weight) = each($this->_config['host'])) 
-		{
-			if(is_string($weight)) {
-				$servers[] = $fromString($weight);
-				continue;
-			}
-			$server    = $fromString($server);
-			$server[]  = $weight;
-			$servers[] = $server;
-		}  
-		
-		return $servers;
+		return $this->connection->expireAt($key, is_int($expiry) ? $expiry : strtotime($expiry));
 	}
 
-	public function write($key, $value, $expiry = null) 
+	public function write($key, $value = null, $expiry = null) 
 	{
 		$connection =& $this->connection;
-		$expiry = ($expiry) ?: $this->_config['expiry'];
+		$expiry     = ($expiry) ?: $this->_config['expiry'];
+		$_self      =& $this;
 
-		return function($self, $params) use (&$connection, $expiry) 
+		return function($self, $params) use (&$_self, &$connection, $expiry) 
 		{
-			$expires = is_int($expiry) ? $expiry : strtotime($expiry);
-			$key     = $params['key'];
+			if(is_array($params['key'])) 
+			{
+				$expiry = $params['data'];
 
-			if(is_array($key))
-				return $connection->setMulti($key, $expires);
-			
-			return $connection->set($key, $params['data'], $expires);
+				if($connection->mset($params['key'])) 
+				{
+					$ttl = array();
+
+					if($expiry) {
+						foreach($params['key'] as $k => $v) {
+							$ttl[$k] = $_self->invokeMethod('_ttl', array($k, $expiry));
+						}
+					}  
+					
+					return $ttl;
+				}
+			}
+			if($result = $connection->set($params['key'], $params['data'])) 
+			{
+				if($expiry)
+					return $_self->invokeMethod('_ttl', array($params['key'], $expiry));
+
+				return $result;
+			}
 		};
 	}
 
@@ -89,13 +79,9 @@ class Memcache extends \arthur\core\Object
 			$key = $params['key'];
 
 			if(is_array($key))
-				return $connection->getMulti($key);
-			if(($result = $connection->get($key)) === false) {
-				if($connection->getResultCode() === Memcached::RES_NOTFOUND)
-					$result = null;
-			}       
+				return $connection->getMultiple($key);
 			
-			return $result;
+			return $connection->get($key);
 		};
 	}
 
@@ -103,9 +89,8 @@ class Memcache extends \arthur\core\Object
 	{
 		$connection =& $this->connection;
 
-		return function($self, $params) use (&$connection) 
-		{
-			return $connection->delete($params['key']);
+		return function($self, $params) use (&$connection) {
+			return (boolean) $connection->delete($params['key']);
 		};
 	}
 
@@ -115,27 +100,27 @@ class Memcache extends \arthur\core\Object
 
 		return function($self, $params) use (&$connection, $offset) 
 		{
-			return $connection->decrement($params['key'], $offset);
+			return $connection->decr($params['key'], $offset);
 		};
 	}
-
+	
 	public function increment($key, $offset = 1) 
 	{
 		$connection =& $this->connection;
 
 		return function($self, $params) use (&$connection, $offset) 
 		{
-			return $connection->increment($params['key'], $offset);
+			return $connection->incr($params['key'], $offset);
 		};
 	}
 
 	public function clear() 
 	{
-		return $this->connection->flush();
+		return $this->connection->flushdb();
 	}
-	
+
 	public static function enabled() 
 	{
-		return extension_loaded('memcached');
+		return extension_loaded('redis');
 	}
 }
