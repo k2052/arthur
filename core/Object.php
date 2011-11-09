c5d062b45f31069dc9e0a36761476b6b4be54685
Ken Erickson <bookworm.productions@gmail.com>
2011-11-08 22:55:41 -0800

    rewrite Core Object

--- core/Object.php
+++ core/Object.php
@@ -3,23 +3,24 @@
 namespace arthur\core;
 
 use arthur\core\Libraries;
-use arthur\util\collection\Filters;  
+use arthur\util\collection\Filters;
 
 class Object 
 {
-  protected $_config = array();
+	protected $_config = array();
 	protected $_autoConfig = array();
 	protected $_methodFilters = array();
 	protected static $_parents = array();
-	
+
 	public function __construct(array $config = array()) 
 	{
 		$defaults = array('init' => true);
 		$this->_config = $config + $defaults;
 
-		if($this->_config['init']) $this->_init();
-	}           
-	
+		if($this->_config['init'])
+			$this->_init();
+	}
+
 	protected function _init() 
 	{
 		foreach($this->_autoConfig as $key => $flag) 
@@ -29,23 +30,23 @@ class Object
 
 			if($flag === 'merge')
 				$this->{"_{$key}"} = $this->_config[$key] + $this->{"_{$key}"};
-			else 
+			else
 				$this->{"_$flag"} = $this->_config[$flag];
 		}
-	}    
-	
+	}
+
 	public function applyFilter($method, $filter = null) 
 	{
 		foreach((array) $method as $m) 
 		{
 			if(!isset($this->_methodFilters[$m]))
-				$this->_methodFilters[$m] = array(); 
-				
+				$this->_methodFilters[$m] = array();
+
 			$this->_methodFilters[$m][] = $filter;
 		}
-	}   
-	
-	public function invokeMethod($method, $params = array())
+	}
+
+	public function invokeMethod($method, $params = array()) 
 	{
 		switch(count($params)) 
 		{
@@ -64,27 +65,28 @@ class Object
 			default:
 				return call_user_func_array(array(&$this, $method), $params);
 		}
-	}   
-	
-	public static function __set_state($data) 
+	}
+
+	public static function __set_state($data)
 	{
-		$class = get_called_class();
+		$class  = get_called_class();
 		$object = new $class();
 
 		foreach($data as $property => $value) {
 			$object->{$property} = $value;
-		}
+		}   
+		
 		return $object;
-	}  
-	
+	}
+
 	protected function _instance($name, array $options = array()) 
 	{
 		if(is_string($name) && isset($this->_classes[$name]))
 			$name = $this->_classes[$name];
 
 		return Libraries::instance(null, $name, $options);
-	}   
-	
+	}
+
 	protected function _filter($method, $params, $callback, $filters = array()) 
 	{
 		list($class, $method) = explode('::', $method);
@@ -92,12 +94,13 @@ class Object
 		if(empty($this->_methodFilters[$method]) && empty($filters))
 			return $callback($this, $params, null);
 
-		$f = isset($this->_methodFilters[$method]) ? $this->_methodFilters[$method] : array();
-		$data = array_merge($f, $filters, array($callback));    
+		$f    = isset($this->_methodFilters[$method]) ? $this->_methodFilters[$method] : array();
+		$data = array_merge($f, $filters, array($callback));   
 		
 		return Filters::run($this, $params, compact('data', 'class', 'method'));
-	}   
-	
+	}
+
+
 	protected static function _parents() 
 	{
 		$class = get_called_class();
@@ -106,8 +109,8 @@ class Object
 			self::$_parents[$class] = class_parents($class);
 
 		return self::$_parents[$class];
-	}  
-	
+	}
+
 	protected function _stop($status = 0) 
 	{
 		exit($status);
